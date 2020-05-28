568d87147e40bb0d768a5b70d75817d5ac0508a4
Kaan İnel <kaaninel@gmail.com>
2020-05-28 22:03:02 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,43 @@
+# Electron + React + Fs + Typescript boilerplate project
+![](./src/resources/project-logo-400.jpg) 
+
+
+#### Template project for desktop apps using [Electron](https://electronjs.org/), [React](https://reactjs.org/) and [Typescript](https://www.typescriptlang.org/).
+
+This project enable access to the **file system** through **Node's fs** library.
+
+---
+<br>
+<br>
+<br>
+
+# Before everything
+As always, install packages:
+```
+npm install
+```
+
+
+# During development
+
+As this project uses [react-hot-loader](https://github.com/gaearon/react-hot-loader) during development, we need to launch [webpack-dev-server](https://github.com/webpack/webpack-dev-server) in a process and Electron in another. 
+<br>
+<br>
+So, in the terminal, run:
+
+```
+npm run server
+```
+then, in another terminal instance:
+```
+npm start
+```
+
+
+# Deploy
+Just run:
+```
+npm run pack
+```
+and the output will be in the ```./pack``` folder.
+
---
+++ src/views/FooView.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import * as fs from "fs";
+
+interface State {
+    text: string;
+}
+
+export default class FooView extends React.Component<RouteComponentProps, State> {
+    fileRef: React.RefObject<HTMLInputElement>;
+
+    constructor(props: RouteComponentProps) {
+        super(props);
+        this.state = {
+            text: "Open a file to display content here"
+        };
+
+        this.fileRef = React.createRef<HTMLInputElement>();
+    }
+
+    onGotoBarClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("Navigating to Bar view");
+        this.props.history.push("/bar");
+    };
+
+    onOpenFileClick = () => {
+        if (this.fileRef.current) {
+            this.fileRef.current.click();
+        }
+    }
+
+    onFileInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        let paths = e.currentTarget.files;
+
+        if (!paths || paths.length < 1) {
+            this.setState({ text: "Could not load files..." });
+            return;
+        }
+
+        let filePath = paths[0].path;
+
+        try {
+            let fileContent = await this.loadFileContent(filePath);
+            this.setState({ text: fileContent });
+        } catch (error) {
+            this.setState({ text: error.message });
+        }
+    }
+
+    loadFileContent = (filePath: string): Promise<string> => {
+        return new Promise<string>((res, rej) => {
+            fs.readFile(filePath, { encoding: "utf8" }, (err, data) => {
+                if (err) {
+                    rej(err);
+                }
+                else {
+                    res(data.toString());
+                }
+            });
+        });
+    }
+
+
+    public render() {
+        return (
+            <div>
+                <h3>FOO View</h3>
+                <p>
+                    {this.state.text}
+                </p>
+
+                <button onClick={this.onOpenFileClick}>Open file...</button>
+                <input type="file" ref={this.fileRef} onChange={this.onFileInputChange} style={{ display: "none" }} />
+                <button onClick={this.onGotoBarClick}>Go to Bar</button>
+            </div>
+        );
+    }
+}
